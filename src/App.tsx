b03b921a0d62c651d046796f27b4ddd37683233d
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-09 16:14:25 +0200

    feat: add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/App.scss'
+import { Header } from './components/Header'
+import { Categories } from './components/Categories'
+import { Sort } from './components/Sort'
+import { Product } from './components/Product'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className='content__title'>All pizzas</h2>
+          <div className='content__items'>
+            <Product title={'Neapolitan Pizza'} price={3} />
+            <Product title={'Chicago Pizza'} price={6} />
+            <Product title={'New York-Style Pizza'} price={5} />
+            <Product title={'Sicilian Pizza'} price={8} />
+            <Product title={'Greek Pizza'} price={4} />
+            <Product title={'California Pizza'} price={5} />
+            <Product title={'Detroit Pizza'} price={7} />
+            <Product title={'St. Louis Pizza'} price={5.5} />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
