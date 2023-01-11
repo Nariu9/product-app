a5af3603d832abfae9a04c61f05f8743a6ab8f52
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-11 21:09:18 +0200

    feat: implement ProductSkeleton

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './scss/App.scss'
 import { Header } from './components/Header'
 import { Categories } from './components/Categories'
 import { Sort } from './components/Sort'
-import { Product } from './components/Product'
-// import products from './assets/data/products.json'
+import { Product } from './components/Product/Product'
+import { ProductSkeleton } from './components/Product/ProductSkeleton'
 
 type ProductType = {
   id: number
@@ -19,11 +19,13 @@ type ProductType = {
 
 function App() {
   const [products, setProducts] = useState<ProductType[]>([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch('https://product-back.vercel.app/')
       setProducts(await data.json())
+      setIsLoading(false)
     }
     fetchData().catch(console.error)
   }, [])
@@ -38,16 +40,18 @@ function App() {
           </div>
           <h2 className='content__title'>All pizzas</h2>
           <div className='content__items'>
-            {products.map((prod: ProductType) => (
-              <Product
-                key={prod.id}
-                img={prod.imageUrl}
-                title={prod.title}
-                price={prod.price}
-                types={prod.types}
-                sizes={prod.sizes}
-              />
-            ))}
+            {isLoading
+              ? [...new Array(8)].map((_, i) => <ProductSkeleton key={i} />)
+              : products.map((prod: ProductType) => (
+                  <Product
+                    key={prod.id}
+                    img={prod.imageUrl}
+                    title={prod.title}
+                    price={prod.price}
+                    types={prod.types}
+                    sizes={prod.sizes}
+                  />
+                ))}
           </div>
         </div>
       </div>
---
+++ src/components/Product/ProductSkeleton.tsx
@@ -0,0 +1,19 @@
+import ContentLoader from 'react-content-loader'
+
+export const ProductSkeleton = () => (
+  <ContentLoader
+    className={'pizza-block'}
+    speed={2}
+    width={280}
+    height={465}
+    viewBox='0 0 280 465'
+    backgroundColor='#f3f3f3'
+    foregroundColor='#ecebeb'
+  >
+    <circle cx='135' cy='125' r='123' />
+    <rect x='40' y='267' rx='10' ry='10' width='198' height='26' />
+    <rect x='2' y='316' rx='10' ry='10' width='277' height='85' />
+    <rect x='0' y='431' rx='10' ry='10' width='102' height='28' />
+    <rect x='139' y='421' rx='15' ry='15' width='141' height='45' />
+  </ContentLoader>
+)
