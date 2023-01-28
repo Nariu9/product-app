d952569a8a5ec133692805ef5d959bb750f18095
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-28 14:02:12 +0200

    fix: fix url

--- src/utils/productsLoader.ts
+++ src/utils/productsLoader.ts
@@ -1,5 +1,5 @@
 export const productsLoader = async () => {
-  const res = await fetch('https://product-back.vercel.app/2')
+  const res = await fetch('https://product-back.vercel.app/')
   if (!res.ok) {
     throw 'Failed to fetch products'
   }
