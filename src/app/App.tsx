d26e9e395fc65afe58cc4824e502c718e9ef6b32
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-28 14:00:29 +0200

    feat: add styles normalize to application

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './scss/App.scss'
-import { Main } from './pages/Main/Main'
+import './App.scss'
+import { Main } from '../pages/Main/Main'
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,14 +8,16 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-import { PageNotFound } from './pages/PageNotFound/PageNotFound'
-import { Root } from './components/Root'
-import { productsLoader } from './utils/productsLoader'
+import { PageNotFound } from '../pages/PageNotFound/PageNotFound'
+import { Root } from '../components/Root'
+import { productsLoader } from '../utils/productsLoader'
+import { Cart } from '../components/Cart/Cart'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={'/'} element={<Root />}>
       <Route index element={<Main />} loader={productsLoader} />
+      <Route path={'/cart'} element={<Cart />} />
       <Route path={'/404'} element={<PageNotFound />} />
       <Route path={'*'} element={<Navigate to={'/404'} />} />
     </Route>
